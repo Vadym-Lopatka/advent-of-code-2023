dbeed15b09e0a4787d90fb7ad95d522b67a7e97d
vlopatka <vadim.lopatka@gmail.com>
2023-12-02 22:24:50 +0200

    day2 part2 done

--- src/vlopatka/advent_of_code_23/day02.clj
+++ src/vlopatka/advent_of_code_23/day02.clj
@@ -2,25 +2,35 @@
   (:require
    [clojure.string :as str]))
 
-(def bag-limit {"red" 12 "blue" 14 "green" 13})
+(def bag {"red" 12 "blue" 14 "green" 13})
 
 (defn parse-info [line]
-  (let [[game-id & info] (re-seq #"\d+|blue|green|red" line)]
+  (let [[game-id & dataset] (re-seq #"\d+|blue|green|red" line)]
     (reduce (fn [m [n k]] (update m k max (parse-long n)))
             {"blue" 0 "green" 0 "red" 0 :game (parse-long game-id)}
-            (partition 2 info))))
+            (partition 2 dataset))))
 
-(defn ?possible [game-info]
-  (->> (merge-with - bag-limit game-info) vals (not-any? neg?)))
+(defn input->games [input] 
+  (->> input slurp str/split-lines (map parse-info)))
+
+(defn power [{:strs [blue green red]}] (* blue green red))
+
+(defn possible? [game]
+  (->> (merge-with - bag game) vals (not-any? neg?)))
 
 (defn part1 [input]
-  (let [games (->> input slurp str/split-lines (map parse-info))]
-    (transduce (comp (filter ?possible) (map :game)) + games)))
+  (let [games (input->games input)]
+    (transduce (comp (filter possible?) (map :game)) + games)))
 
+(defn part2 [input]
+  (let [games (input->games input)]
+    (transduce (map power) + games) ))
 
 (comment
-
+  (part1 "resources/day02-test-input.txt")
   (part1 "resources/day02-input.txt")
+  (part2 "resources/day02-input.txt")
+  
 
   (def line "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
   (re-seq #"\w+|\;" line)

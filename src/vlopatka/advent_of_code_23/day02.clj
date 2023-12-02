5b1a83e4b6d510b0a8826c1c42323079925bea62
vlopatka <vadim.lopatka@gmail.com>
2023-12-02 21:44:01 +0200

    day2 p1 done

--- src/vlopatka/advent_of_code_23/day02.clj
+++ src/vlopatka/advent_of_code_23/day02.clj
@@ -1,45 +1,31 @@
 (ns vlopatka.advent-of-code-23.day02
   (:require
-   [clojure.edn :as edn]
    [clojure.string :as str]))
 
+(def bag-limit {"red" 12 "blue" 14 "green" 13})
 
-(defn old-part1 [input]
-  (apply +
-         (for [line (str/split-lines input)]
-           (let [digits (re-seq #"\d" line)
-                 number (str (first digits) (last digits))]
-             (edn/read-string number)))))
+(defn parse-info [line]
+  (let [[game-id & info] (re-seq #"\d+|blue|green|red" line)]
+    (reduce (fn [m [n k]] (update m k max (parse-long n)))
+            {"blue" 0 "green" 0 "red" 0 :game (parse-long game-id)}
+            (partition 2 info))))
 
-
-(def bag-conf {"red" 12
-               "blue" 14
-               "green" 13})
-
-(defn possible-game? 
-  [game]
-  true)
+(defn ?possible [game-info]
+  (->> (merge-with - bag-limit game-info) vals (not-any? neg?)))
 
 (defn part1 [input]
-  (apply +
-         (for [line (str/split-lines input)]
-           (let [line-as-list (re-seq #"\w+" line)
-                 game-id (second line-as-list)
-                 possible? (possible-game? line)]
-             (if possible? (edn/read-string game-id) 0)))))
+  (let [games (->> input slurp str/split-lines (map parse-info))]
+    (transduce (comp (filter ?possible) (map :game)) + games)))
+
 
 (comment
-  
-  (def day02-input (slurp "resources/day02-input.txt"))
 
-  (part1 day02-input)~
-  
+  (part1 "resources/day02-input.txt")
+
   (def line "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
-
-  (re-seq #"\d" line)
   (re-seq #"\w+|\;" line)
-;;   ("Game" "1" "3" "blue" "4" "red" ";" "1" "red" "2" "green" "6" "blue" ";" "2" "green")
-  
-  (.split line ":")
-  
+  (re-seq #"\d+|blue|green|red" line)
+
+  (parse-info line)
+
   )

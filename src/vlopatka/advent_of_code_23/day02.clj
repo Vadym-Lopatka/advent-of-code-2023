74565da22962f35868bcfe5b038dbf000f6e4195
vlopatka <vadim.lopatka@gmail.com>
2023-12-02 18:20:57 +0200

    day02 in the middle of day02p1

--- src/vlopatka/advent_of_code_23/day02.clj
+++ src/vlopatka/advent_of_code_23/day02.clj
@@ -4,13 +4,42 @@
    [clojure.string :as str]))
 
 
-(defn part1 [input]
+(defn old-part1 [input]
   (apply +
          (for [line (str/split-lines input)]
            (let [digits (re-seq #"\d" line)
                  number (str (first digits) (last digits))]
              (edn/read-string number)))))
 
+
+(def bag-conf {"red" 12
+               "blue" 14
+               "green" 13})
+
+(defn possible-game? 
+  [game]
+  true)
+
+(defn part1 [input]
+  (apply +
+         (for [line (str/split-lines input)]
+           (let [line-as-list (re-seq #"\w+" line)
+                 game-id (second line-as-list)
+                 possible? (possible-game? line)]
+             (if possible? (edn/read-string game-id) 0)))))
+
 (comment
+  
+  (def day02-input (slurp "resources/day02-input.txt"))
+
+  (part1 day02-input)~
+  
+  (def line "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+
+  (re-seq #"\d" line)
+  (re-seq #"\w+|\;" line)
+;;   ("Game" "1" "3" "blue" "4" "red" ";" "1" "red" "2" "green" "6" "blue" ";" "2" "green")
+  
+  (.split line ":")
   
   )
